1f7233b496f17d308f908a03649fc821c391bb31
Martin Brunot <brmartin@esiea.fr>
2025-06-18 23:29:43 +0200

    api and search books

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
+import requests
 
 main = Blueprint('main', __name__)
 
@@ -8,3 +9,15 @@ main = Blueprint('main', __name__)
 def home():
     return render_template('home.html', user=current_user)
 
+@main.route('/search', methods=['GET', 'POST'])
+def search():
+    books = []
+    if request.method == 'POST':
+        query = request.form.get('query')
+        if query:
+            url = f'https://www.googleapis.com/books/v1/volumes?q={query}'
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                books = data.get('items', [])
+    return render_template('search.html', books=books)
