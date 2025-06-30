b21dfcf802bad2f30b62ba54da5a2af73bb28a70
lfrancois75 <lucas.francois@rudder.io>
2025-06-30 14:14:32 +0200

    English traduction for HTML and CSS comments

--- app/models.py
+++ app/models.py
@@ -23,7 +23,6 @@ class Comment(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-
     user = db.relationship('User', backref='comments')
 
 class BookLike(db.Model):
